496398b3ba758ca4dd51636caeea48d6581ace7a
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-26 11:39:41 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Texto vazio Atividade web
+
+String Texto;
+Console.WriteLine("----TExto Vazio----");
+
+Console.Write("Digite um Texto:");
+Texto = Console.ReadLine()!;
+
+Console.WriteLine(String.IsNullOrEmpty(Texto.Trim()));
